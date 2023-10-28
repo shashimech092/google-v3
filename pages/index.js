098f691f5c2138adc23a9da5fc6999552c0515b5
Shashikant Lamani <shashimech092@gmail.com>
2023-10-28 18:48:44 +0530

    create footer component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Head from "next/head";
 import Image from "next/image";
 import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -29,6 +30,7 @@ export default function Home() {
         <button className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
+      <Footer/>
     </div>
   );
 }
